e2abc11e02956f32b225046be09e68331c37f8d4
Franco Escobar <franco_m77@gmail.com>
2014-12-11 09:35:21 -0300

    Interfaces provisorias

--- reservalacancha/urls.py
+++ reservalacancha/urls.py
@@ -5,5 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', 'website.views.index'),
+    url(r'^$', 'website.views.index'),
+    url(r'^list/$', 'website.views.list'),
+    url(r'^detail/$', 'website.views.detail'),
+    url(r'^reservas/$', 'website.views.mis_res'),
+    url(r'^login/$', 'website.views.login'),
 )
--- website/views.py
+++ website/views.py
@@ -5,3 +5,23 @@ def index(request):
 
  return render_to_response('index.html',
         context_instance=RequestContext(request))
+
+def list(request):
+
+	return render_to_response('list.html',
+		context_instance=RequestContext(request))
+
+def detail(request):
+	
+	return render_to_response('detail.html',
+		context_instance=RequestContext(request))
+
+def mis_res(request):
+	
+	return render_to_response('list_res.html',
+		context_instance=RequestContext(request))
+
+def login(request):
+	
+	return render_to_response('login.html',
+		context_instance=RequestContext(request))
