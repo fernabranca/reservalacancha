b3db991e8897f63353c0e7aa2e5912cbb2c6095c
francom77 <franco_m77@hotmail.com>
2015-03-04 10:53:49 -0300

    cambiado mail de envio de correos

--- reservalacancha/settings.py
+++ reservalacancha/settings.py
@@ -100,5 +100,5 @@ LOGIN_URL = '/login/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'reservalachancha@gmail.com'
-EMAIL_HOST_PASSWORD = 'sistemadereservasonline'
+EMAIL_HOST_USER = 'reservadecanchasonline@gmail.com'
+EMAIL_HOST_PASSWORD = 'sistemadereservas'
